6b925c122b3216c0fe41d50b9b9978f67f502719
Marciano Renzo William <marcianorenzowilliam@gmail.com>
2022-06-18 01:10:37 +0700

    Added Header, Location, and search bar

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
 import {
   Dimensions,
   Image,
@@ -6,19 +7,31 @@ import {
   StyleSheet,
   Text,
   View,
+  Alert,
   FlatList,
   ScrollView,
   TextInput,
   TouchableHighlight,
   TouchableOpacity,
 } from 'react-native';
+
+// Icons
+import { Octicons } from '@expo/vector-icons';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import categories from './../components/categories';
 import foods from './../components/foods';
 import {
-  HomeContainer,
+  StyledContainer,
   HeaderHome,
-  InputContainer,
+  Greetings,
+  ProfilePictureHolder,
+  ProfilePicture,
+  StyledFormArea,
+  StyledTextInput,
+  LeftIcon,
+  LocationHolder,
+  BodyOneHome,
   SortBtn,
   CategoriesListContainer,
   CategoryBtn,
@@ -29,130 +42,117 @@ import {
 } from '../components/styles';
 
 // colors
-const {brand, darkLight, tertiary, primary} = Colors;
+const {
+    brand, 
+    darkLight, 
+    tertiary, 
+    primary, 
+    secondary,
+    red
+} = Colors;
+
+// Firebase
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+const db = firebase.firestore();
+/*
+const docRef = db.collection("users").doc(firebase.auth().currentUser.uid);
+// Getting user data
+const data = docRef.get().then((doc) => {
+  if (doc.exists) {
+      console.log("Document data:", doc.data());
+  } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+  }
+}).catch((error) => {
+  console.log("Error getting document:", error);
+});
+*/
 
 const HomePage = ({navigation}) => {
-  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0);
+    const onPressLogOut = () => {
+        Alert.alert(
+            "Log Out",
+            "Are you sure you want to log out?",
+            [{
+                text: "Cancel",
+                onPress: () => {
+                    navigation.navigate("HomePage");
+                    console.log('User decided not to log out');
+                },
+                style: 'cancel'
+            }, {
+                text: 'Log Out',
+                onPress: onSigningOut ,
+            }]
+        );
+    }
 
-  const ListCategories = () => {
+    const onSigningOut = () => {
+        firebase.auth()
+          .signOut()
+          .then(() => console.log('User signed out!'));
+    }
+
+    return(
+        <StyledContainer home={true}>
+        <StatusBar style="dark" />
+            <HeaderHome>
+                <View>
+                    <View style={{flexDirection: "row"}}>
+                        <Greetings>Best Foods </Greetings>
+                    {/* <Greetings user={true}>user</Greetings> */ } 
+                    </View>
+                    <Greetings>Near You! </Greetings>
+                    { /*
+                    <Greetings sub={true}>
+                        What do you want to eat today?
+                    </Greetings>
+                    */}
+                </View>
+                <ProfilePictureHolder
+                    onPress={onPressLogOut}
+                >
+                    <ProfilePicture 
+                        source={require('./../assets/avatar.jpg')}
+                    />
+                </ProfilePictureHolder>
+            </HeaderHome>
+            <LocationHolder>
+                <Octicons 
+                    name="location"
+                    size={20}
+                    color={red}
+                    style={{paddingRight:10}} 
+                    />
+                <Greetings sub={true}>Kent Ridge, Singapore</Greetings>
+            </LocationHolder>
+            <BodyOneHome>
+                <StyledFormArea search={true}>
+                    <MyTextInput 
+                        icon="search"
+                        placeholder="Search here"
+                    />
+                </StyledFormArea>
+                <SortBtn>
+                    <Icon name="tune" size={28} color={primary} />
+                </SortBtn>
+            </BodyOneHome>
+
+        </StyledContainer>
+    )
+}
+
+const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
     return (
-      <CategoriesListContainer
-        horizontal
-        showsHorizontalScrollIndicator={false}>
-        {categories.map((category, index) => (
-          <TouchableOpacity
-            key={index}
-            activeOpacity={0.8}
-            onPress={() => setSelectedCategoryIndex(index)}>
-            <CategoryBtn>
-              <CategoryBtnImgCon>
-                <Image
-                  source={require('./../assets/LogoOnly.png')}
-                  style={{height: 35, width: 35, resizeMode: 'cover'}}
-                />
-              </CategoryBtnImgCon>
-              <Text
-                style={{
-                  fontSize: 15,
-                  fontWeight: 'bold',
-                  marginLeft: 10,
-                  color:
-                    selectedCategoryIndex == index
-                      ? {primary}
-                      : {brand}
-                }}>
-                {category.name}
-              </Text>
-            </CategoryBtn>
-          </TouchableOpacity>
-        ))}
-      </CategoriesListContainer>
-    );
-  };
-
-  const Card = ({food}) => {
-    return (
-      <TouchableHighlight
-        underlayColor={primary}
-        activeOpacity={0.9}
-        // onPress={() => navigation.navigate('DetailsScreen', food)}
-        >
-        <CardHome>
-          <View style={{alignItems: 'center', top: -40}}>
-            <Image source={food.image} style={{height: 120, width: 120}} />
-          </View>
-          <View style={{marginHorizontal: 20}}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>{food.name}</Text>
-            <Text style={{fontSize: 14, color: {tertiary}, marginTop: 2}}>
-              {food.ingredients}
-            </Text>
-          </View>
-          <View
-            style={{
-              marginTop: 10,
-              marginHorizontal: 20,
-              flexDirection: "row",
-              justifyContent: 'space-between',
-            }}>
-            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-              ${food.price}
-            </Text>
-            <AddToCartBtn>
-              <Icon name="add" size={20} color={primary} />
-            </AddToCartBtn>
-          </View>
-        </CardHome>
-      </TouchableHighlight>
-    );
-  };
-
-  return (
-    <HomeContainer>
-      <HeaderHome>
-        <View>
-          <View style={{flexDirection: "row"}}>
-            <Text style={{fontSize: 28}}>Hello,</Text>
-            <Text style={{fontSize: 28, fontWeight: 'bold', marginLeft: 10}}>
-              Marco
-            </Text>
-          </View>
-          <Text style={{marginTop: 5, fontSize: 22, color: {tertiary}}}>
-            What do you want to eat today?
-          </Text>
-        </View>
-        <Image
-          source={require('./../assets/avatar.jpg')}
-          style={{height: 50, width: 50, borderRadius: 25}}
-        />
-      </HeaderHome>
-      <View
-        style={{
-          marginTop: 40,
-          flexDirection: "row",
-          paddingHorizontal: 20,
-        }}>
-        <InputContainer>
-          <Icon name="search" size={25} style={{paddingRight:10}} />
-          <TextInput
-            style={{flex: 1, fontSize: 18}}
-            placeholder="Search for food"
-          />
-        </InputContainer>
-        <SortBtn>
-          <Icon name="tune" size={28} color={primary} />
-        </SortBtn>
-      </View>
       <View>
-        <ListCategories />
-      </View>
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        numColumns={2}
-        data={foods}
-        renderItem={({item}) => <Card food={item} />}
-      />
-    </HomeContainer>
-  );
+        <LeftIcon search={true}>
+            <Octicons name={icon} size={25} color={darkLight} />
+        </LeftIcon>        
+        <StyledTextInput {...props}/>
+      </View>);
 };
 
 export default HomePage;
